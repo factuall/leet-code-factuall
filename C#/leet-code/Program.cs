57f04e69c3e1ccc5a0610f4b58a9a346b5b45cd9
Adrian Nieściur <63786641+factuall@users.noreply.github.com>
2023-10-01 21:07:06 +0200

    Basic setup for solving C# problems

---
+++ C#/leet-code/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using leet_code.problems;
+
+namespace leet_code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (TestCase test in Valid_parentheses.testCases)
+            {
+                test.checkCase(Valid_parentheses.IsValid(test.tCase));
+            }
+        }
+    }
+
+    public class TestCase
+    {
+        public dynamic tCase;
+        dynamic tResult;
+
+        public TestCase(string tCase, bool tResult)
+        {
+            this.tCase = tCase;
+            this.tResult = tResult;
+        }
+
+        public void checkCase(dynamic inputResult)
+        {
+            
+            //temporary solution
+            if (inputResult.GetType() == tResult.GetType())
+            {
+                string textCase = "";
+                if(tCase.GetType().FullName == "System.String")
+                {
+                    textCase = tCase;
+                }else if (tCase.GetType().FullName.EndsWith("[]"))
+                {
+
+                }
+                else
+                {
+                    textCase = tCase.ToString();
+                }
+                if (inputResult = tResult) Console.WriteLine(
+                    "Test Status: " +
+                    ((inputResult = tResult) ? "Passed!" : "Failed!") +
+                    " for following data input: " + tCase);
+            }
+
+        }
+    }
+}
---
+++ C#/leet-code/problems/Valid-parentheses.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace leet_code.problems
+{
+    internal class Valid_parentheses
+    {
+        //TEST CASES
+        public static TestCase[] testCases =
+        {
+            new TestCase("s", true),
+            new TestCase("d", true),
+        };
+        
+
+        public static bool IsValid(string s)
+        {
+
+            return true;
+        }
+
+    }
+}
